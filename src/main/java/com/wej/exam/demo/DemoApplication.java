b15bee2cec0ea8641ae977f3fa97815797007198
Eunju0114 <wangeunju0114@gmail.com>
2022-04-21 20:50:53 +0900

    notebook

---
+++ src/main/java/com/wej/exam/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.wej.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Data;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@Data
+class Article{
+	
+	private int id;
+}
+
